7794cb3e006ce1d5321c3e03ebb1868282916011
bettercallamir <133879857+bettercallamir@users.noreply.github.com>
2026-08-05 11:16:33 +0330

    [ADD] button component

--- src/components/TextBar.jsx
+++ src/components/TextBar.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Button from './Button'
 
 export default function TextBar() {
   return (
-    <div className={`overflow-hidden pl-10 pt-10 flex flex-col w-[45dvw] h-full text-white`}>
-        <div className='flex items-center rounded-md p-4 border-2 border-white gap-2 w-[120px] h-[35px] text-sm text-center '>
-            <span className='w-[10px] h-[10px] bg-green-300 rounded-full animate-pulse'></span>
-            در درسترس</div>
+    <div className={`overflow-hidden pt-10 flex flex-col w-[45dvw] h-full text-white bg-gray-800`}>
+        <div className='lg:ml-[95px] flex items-center rounded-md p-4 border-1 border-[#EBEBEB] gap-2 w-[120px] h-[35px] text-sm text-center '>
+            <span className='w-[10px] h-[10px] bg-[#6DFF8D] rounded-full animate-pulse'></span>
+         در درسترس</div>
+
+        <div className='w-full flex flex-col  pt-8 text-5xl font-bold items-end text-right'>
+            <h1>طراحی و توسعه وب</h1>
+            <h2>مدرن</h2>
+        </div>
+
+        <div className='w-full flex flex-col  pt-8 text-xl items-end text-right'>
+            <p>وب‌سایت، داشبورد و پنل‌های اختصاصی با تمرکز
+بر سرعت، تجربه کاربری و کیفیت</p>
+        </div>
+
+        <div className='flex gap-2 w-full justify-end pt-[9px] '>
+            <Button text={"شروع همکاری"} type={0}/>
+            <Button text={"مشاهده نمونه کار"} type={1}></Button>
+        </div>
         
     </div>
   )
